e9ec727f393ea9a030684207045a7d73b921e01e
3052 <spen+3052@protonmail.com>
2024-06-29 11:54:49 -0500

    +const start = 0

--- go/umber-check/umber-check.go
+++ go/umber-check/umber-check.go
@@ -9,6 +9,8 @@ import (
    "time"
 )
 
+const start = 0
+
 func main() {
    file, err := os.Open("../../docs/umber.json")
    if err != nil {
@@ -20,23 +22,24 @@ func main() {
    }
    json.NewDecoder(file).Decode(&songs)
    for i, song := range songs {
-      if i >= 1033 {
-         query, err := url.ParseQuery(song.Q)
+      if i < start {
+         continue
+      }
+      query, err := url.ParseQuery(song.Q)
+      if err != nil {
+         panic(err)
+      }
+      if query.Get("p") == "y" {
+         var req youtube.Request
+         req.VideoId = query.Get("b")
+         req.Android()
+         var play youtube.Player
+         err := play.Post(req, nil)
          if err != nil {
             panic(err)
          }
-         if query.Get("p") == "y" {
-            var req youtube.Request
-            req.VideoId = query.Get("b")
-            req.Android()
-            var play youtube.Player
-            err := play.Post(req, nil)
-            if err != nil {
-               panic(err)
-            }
-            fmt.Println(play.PlayabilityStatus.Status, req.VideoId, len(songs)-i)
-            time.Sleep(99*time.Millisecond)
-         }
+         fmt.Println(play.PlayabilityStatus.Status, req.VideoId, len(songs)-i)
+         time.Sleep(99*time.Millisecond)
       }
    }
 }
