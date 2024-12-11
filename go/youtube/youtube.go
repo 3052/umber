f69c97607b1ad49b537983d01916b130ba3e108a
3052 <spen+3052@protonmail.com>
2024-12-11 17:32:58 -0600

    deleted:    go/youtube/token.go

--- go/youtube/youtube.go
+++ go/youtube/youtube.go
@@ -1,12 +1,130 @@
 package youtube
 
 import (
+   "bytes"
+   "encoding/json"
    "errors"
    "fmt"
    "mime"
+   "net/http"
+   "net/url"
    "strings"
+   "time"
 )
 
+func (i *InnerTube) Player(token *AuthToken) (*Player, error) {
+   i.Context.Client.AndroidSdkVersion = 32
+   i.Context.Client.OsVersion = "12"
+   switch i.Context.Client.ClientName {
+   case android:
+      i.ContentCheckOk = true
+      i.Context.Client.ClientVersion = android_version
+      i.RacyCheckOk = true
+   case android_embedded_player:
+      i.Context.Client.ClientVersion = android_version
+   case web:
+      i.Context.Client.ClientVersion = web_version
+   }
+   data, err := json.Marshal(i)
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://www.youtube.com/youtubei/v1/player",
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("user-agent", user_agent + i.Context.Client.ClientVersion)
+   if token != nil {
+      req.Header.Set("authorization", "Bearer " + token.AccessToken)
+   }
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   play := &Player{}
+   err = json.NewDecoder(resp.Body).Decode(play)
+   if err != nil {
+      return nil, err
+   }
+   return play, nil
+}
+
+type Player struct {
+   Microformat struct {
+      PlayerMicroformatRenderer struct {
+         PublishDate Date
+      }
+   }
+   PlayabilityStatus struct {
+      Status string
+      Reason string
+   }
+   StreamingData struct {
+      AdaptiveFormats []AdaptiveFormat
+   }
+   VideoDetails struct {
+      Author string
+      LengthSeconds int64 `json:",string"`
+      ShortDescription string
+      Title string
+      VideoId string
+      ViewCount int64 `json:",string"`
+   }
+}
+
+const user_agent = "com.google.android.youtube/"
+
+type Date struct {
+   Time time.Time
+}
+
+func (d *Date) UnmarshalText(text []byte) error {
+   var err error
+   d.Time, err = time.Parse(time.RFC3339, string(text))
+   if err != nil {
+      return err
+   }
+   return nil
+}
+
+// YouTube on TV
+const (
+   client_id = "861556708454-d6dlm3lh05idd8npek18k6be8ba3oc68.apps.googleusercontent.com"
+   client_secret = "SboVhoG9s0rNafixCSGGKXAT"
+)
+
+func (a *AuthToken) Refresh() error {
+   resp, err := http.PostForm(
+      "https://oauth2.googleapis.com/token", url.Values{
+         "client_id": {client_id},
+         "client_secret": {client_secret},
+         "grant_type": {"refresh_token"},
+         "refresh_token": {a.RefreshToken},
+      },
+   )
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   return json.NewDecoder(resp.Body).Decode(a)
+}
+
+func (a *AuthToken) Marshal() ([]byte, error) {
+   return json.Marshal(a)
+}
+
+type AuthToken struct {
+   AccessToken string `json:"access_token"`
+   RefreshToken string `json:"refresh_token"`
+}
+
+func (a *AuthToken) Unmarshal(text []byte) error {
+   return json.Unmarshal(text, a)
+}
 // need `osVersion` this to get the correct:
 // This video requires payment to watch
 // instead of the invalid:
