7b0d21db16ea431b64a7fba524ea777bb78ebd7d
3052 <spen+3052@protonmail.com>
2024-12-21 07:14:41 -0600

    deleted:    umber-check/main.go

--- umber-check/umber-check.go
+++ umber-check/umber-check.go
@@ -4,9 +4,58 @@ import (
    "bytes"
    "encoding/json"
    "errors"
+   "flag"
+   "fmt"
    "net/http"
+   "net/url"
+   "os"
+   "time"
 )
 
+func main() {
+   start := flag.Int("s", -1, "start")
+   flag.Parse()
+   if *start <= -1 {
+      flag.Usage()
+      return
+   }
+   file, err := os.Open("umber.json")
+   if err != nil {
+      panic(err)
+   }
+   defer file.Close()
+   var songs []struct {
+      Q string
+      S string
+   }
+   err = json.NewDecoder(file).Decode(&songs)
+   if err != nil {
+      panic(err)
+   }
+   for i, song := range songs {
+      if i < *start {
+         continue
+      }
+      query, err := url.ParseQuery(song.Q)
+      if err != nil {
+         panic(err)
+      }
+      if query.Get("p") == "y" {
+         var tube InnerTube
+         tube.VideoId = query.Get("b")
+         play, err := tube.Player()
+         if err != nil {
+            panic(err)
+         }
+         fmt.Println(i, len(songs), tube.VideoId, song.S)
+         if play.PlayabilityStatus.Status != "OK" {
+            fmt.Println(play.PlayabilityStatus.Status)
+            break
+         }
+         time.Sleep(99 * time.Millisecond)
+      }
+   }
+}
 func (i *InnerTube) Player() (*Player, error) {
    i.ContentCheckOk = true
    i.RacyCheckOk = true
