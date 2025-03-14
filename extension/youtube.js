3a4143b520e275f25cb378cef800115523410dce
367 <367@tuta.io>
2025-03-14 10:44:42 -0500

    new file:   internal/youtube.go

--- extension/youtube.js
+++ extension/youtube.js
@@ -2,21 +2,23 @@
 
 async function youTube() {
    const ref = new URL(this.href);
-   const body = {
-      context: {
-         client: {
-            androidSdkVersion: 99,
-            clientName: 'ANDROID',
-            clientVersion: '18.19.99'
-         }
-      },
-      videoId: ref.searchParams.get('v')
-   };
-   const req = {
-      body: JSON.stringify(body),
-      headers: {'user-agent': 'com.google.android.youtube/18.99.99'},
-      method: 'POST'
-   };
+   const req = {};
+   const body = {};
+   body.videoId = ref.searchParams.get('v');
+   body.context = {};
+   body.context.client = {};
+   req.headers = {};
+   //////////////////////////////////////////////////////////////////////////////
+   //body.context.client.androidSdkVersion = 99;
+   //body.context.client.clientName = 'ANDROID';
+   body.context.client.clientName = 'IOS';
+   //body.context.client.clientVersion = '18.19.99';
+   body.context.client.clientVersion = '20.03.02';
+   //req.headers['user-agent'] = 'com.google.android.youtube/18.99.99';
+   req.headers['user-agent'] = 'com.google.ios.youtube/20.03.02 (iPhone16,2; U; CPU iOS 18_2_1 like Mac OS X;)';
+   //////////////////////////////////////////////////////////////////////////////
+   req.body = JSON.stringify(body);
+   req.method = 'POST';
    const resp = await fetch('https://www.youtube.com/youtubei/v1/player', req);
    const play = await resp.json();
    const msg = {
---
+++ internal/go.mod
@@ -0,0 +1,7 @@
+module internal
+
+go 1.24.0
+
+require 41.neocities.org/protobuf v1.4.6
+
+require google.golang.org/protobuf v1.36.5 // indirect
