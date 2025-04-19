cc16640b82f6f50f8aec9fd40dc93bc8e2527224
367 <367@tuta.io>
2025-04-19 17:17:49 -0500

    +type YtImg struct {

--- go/insert-youtube/youtube.go
+++ go/insert-youtube/youtube.go
@@ -16,224 +16,6 @@ import (
    "umber/youtube"
 )
 
-type Player struct {
-   Microformat struct {
-      PlayerMicroformatRenderer struct {
-         PublishDate Date
-      }
-   }
-   PlayabilityStatus struct {
-      Status string
-      Reason string
-   }
-   VideoDetails struct {
-      Author           string
-      LengthSeconds    int64 `json:",string"`
-      ShortDescription string
-      Title            string
-      VideoId          string
-      ViewCount        int64 `json:",string"`
-   }
-}
-
-func (d *Date) UnmarshalText(data []byte) error {
-   var err error
-   d[0], err = time.Parse(time.RFC3339, string(data))
-   if err != nil {
-      return err
-   }
-   return nil
-}
-
-type Date [1]time.Time
-
-type song struct {
-   Q string
-   S string
-}
-
-func read_songs(name string) ([]*song, error) {
-   data, err := os.ReadFile(name)
-   if err != nil {
-      return nil, err
-   }
-   var songs []*song
-   err = json.Unmarshal(data, &songs)
-   if err != nil {
-      return nil, err
-   }
-   return songs, nil
-}
-
-///
-
-func main() {
-   flag.Parse()
-   songs, err := read_songs("umber.json")
-   if err != nil {
-      panic(err)
-   }
-   if len(os.Args) >= 3 {
-      args := os.Args[2:]
-      var song1 *song
-      song1, err = new_youtube().parse(args)
-      if err != nil {
-         panic(err)
-      }
-      songs = append([]*song{song1}, songs...)
-      var buf bytes.Buffer
-      enc := json.NewEncoder(&buf)
-      enc.SetEscapeHTML(false)
-      enc.SetIndent("", " ")
-      err := enc.Encode(songs)
-      if err != nil {
-         panic(err)
-      }
-      err = os.WriteFile("umber.json", buf.Bytes(), os.ModePerm)
-      if err != nil {
-         panic(err)
-      }
-   } else {
-      new_http().f.Usage()
-      new_bandcamp().f.Usage()
-      new_soundcloud().f.Usage()
-      new_youtube().f.Usage()
-   }
-}
-
-func (y *youtube_set) parse(args []string) (*song, error) {
-   y.f.Parse(args)
-   now := strconv.FormatInt(time.Now().Unix(), 36)
-   value := url.Values{}
-   value.Set("a", now)
-   value.Set("p", "y")
-   value.Set("b", y.tube.VideoId)
-   base, err := get_image(y.tube.VideoId)
-   if err != nil {
-      return nil, err
-   }
-   if base != "" {
-      value.Set("c", base)
-   }
-   play, err := y.tube.Player()
-   if err != nil {
-      return nil, err
-   }
-   var song1 song
-   song1.S = play.VideoDetails.Author + " - " + play.VideoDetails.Title
-   fmt.Println(play.VideoDetails.ShortDescription)
-   value.Set("y", strconv.Itoa(
-      play.Microformat.PlayerMicroformatRenderer.PublishDate[0].Year(),
-   ))
-   song1.Q = value.Encode()
-   return &song1, nil
-}
-
-func new_youtube() *youtube_set {
-   set.f.StringVar(&set.tube.VideoId, "b", "", "video ID")
-   set.tube.Context.Client.ClientName = "WEB"
-   return &set
-}
-
-func get_image(video_id string) (string, error) {
-   var imgs []youtube.YtImg
-   for _, img := range youtube.YtImgs {
-      if img.Height < 720 {
-         imgs = append(imgs, img)
-      }
-   }
-   sort.SliceStable(imgs, func(a, b int) bool {
-      com := imgs[a].Height - imgs[b].Height
-      if com != 0 {
-         return com >= 1
-      }
-      def := func(i int) int {
-         return strings.Index(imgs[i].Name, "default")
-      }
-      com = def(a) - def(b)
-      if com != 0 {
-         return com >= 1
-      }
-      def = func(i int) int {
-         return strings.Index(imgs[i].Name, "webp")
-      }
-      return def(b) < def(a)
-   })
-   for index, img := range imgs {
-      img.VideoId = video_id
-      address := img.String()
-      fmt.Println(address)
-      resp, err := http.Head(address)
-      if err != nil {
-         return "", err
-      }
-      if resp.StatusCode == http.StatusOK {
-         if index == 0 {
-            return "", nil
-         }
-         return path.Base(address), nil
-      }
-   }
-   return "", nil
-}
-
-func (i *InnerTube) Player() (*Player, error) {
-   i.Context.Client.AndroidSdkVersion = 32
-   i.Context.Client.OsVersion = "12"
-   switch i.Context.Client.ClientName {
-   case android:
-      i.ContentCheckOk = true
-      i.Context.Client.ClientVersion = android_version
-      i.RacyCheckOk = true
-   case android_embedded_player:
-      i.Context.Client.ClientVersion = android_version
-   case web:
-      i.Context.Client.ClientVersion = web_version
-   }
-   data, err := json.Marshal(i)
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://www.youtube.com/youtubei/v1/player",
-      bytes.NewReader(data),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("user-agent", user_agent+i.Context.Client.ClientVersion)
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   play := &Player{}
-   err = json.NewDecoder(resp.Body).Decode(play)
-   if err != nil {
-      return nil, err
-   }
-   return play, nil
-}
-
-// need `osVersion` this to get the correct:
-// This video requires payment to watch
-// instead of the invalid:
-// This video can only be played on newer versions of Android or other
-// supported devices.
-type InnerTube struct {
-   ContentCheckOk bool `json:"contentCheckOk,omitempty"`
-   Context        struct {
-      Client struct {
-         AndroidSdkVersion int    `json:"androidSdkVersion"`
-         ClientName        string `json:"clientName"`
-         ClientVersion     string `json:"clientVersion"`
-         OsVersion         string `json:"osVersion"`
-      } `json:"client"`
-   } `json:"context"`
-   RacyCheckOk bool   `json:"racyCheckOk,omitempty"`
-   VideoId     string `json:"videoId"`
-}
-
 type YtImg struct {
    Height  int
    Name    string
@@ -288,26 +70,214 @@ var YtImgs = []YtImg{
    {Width: 1280, Height: 720, Name: "maxres3.webp"},
 }
 
-func (y *YtImg) String() string {
-   var data strings.Builder
-   data.WriteString("http://i.ytimg.com/vi")
-   if strings.HasSuffix(y.Name, ".webp") {
-      data.WriteString("_webp")
+type Player struct {
+   Microformat struct {
+      PlayerMicroformatRenderer struct {
+         PublishDate Date
+      }
    }
-   data.WriteByte('/')
-   data.WriteString(y.VideoId)
-   data.WriteByte('/')
-   data.WriteString(y.Name)
-   return data.String()
+   PlayabilityStatus struct {
+      Status string
+      Reason string
+   }
+   VideoDetails struct {
+      Author           string
+      LengthSeconds    int64 `json:",string"`
+      ShortDescription string
+      Title            string
+      VideoId          string
+      ViewCount        int64 `json:",string"`
+   }
 }
 
-const (
-   android_version = "19.33.35"
-   web_version     = "2.20231219.04.00"
-)
+func (d *Date) UnmarshalText(data []byte) error {
+   var err error
+   d[0], err = time.Parse(time.RFC3339, string(data))
+   if err != nil {
+      return err
+   }
+   return nil
+}
 
-const (
-   android                 = "ANDROID"
-   android_embedded_player = "ANDROID_EMBEDDED_PLAYER"
-   web                     = "WEB"
-)
+type Date [1]time.Time
+
+type song struct {
+   Q string
+   S string
+}
+
+func read_songs(name string) ([]*song, error) {
+   data, err := os.ReadFile(name)
+   if err != nil {
+      return nil, err
+   }
+   var songs []*song
+   err = json.Unmarshal(data, &songs)
+   if err != nil {
+      return nil, err
+   }
+   return songs, nil
+}
+
+func main() {
+   flag.Parse()
+   songs, err := read_songs("umber.json")
+   if err != nil {
+      panic(err)
+   }
+   if len(os.Args) >= 3 {
+      args := os.Args[2:]
+      var song1 *song
+      song1, err = new_youtube().parse(args)
+      if err != nil {
+         panic(err)
+      }
+      songs = append([]*song{song1}, songs...)
+      var buf bytes.Buffer
+      enc := json.NewEncoder(&buf)
+      enc.SetEscapeHTML(false)
+      enc.SetIndent("", " ")
+      err := enc.Encode(songs)
+      if err != nil {
+         panic(err)
+      }
+      err = os.WriteFile("umber.json", buf.Bytes(), os.ModePerm)
+      if err != nil {
+         panic(err)
+      }
+   } else {
+      flag.Usage()
+   }
+}
+
+const web_version     = "2.20231219.04.00"
+
+func (y *youtube_set) parse(args []string) (*song, error) {
+   y.f.Parse(args)
+   now := strconv.FormatInt(time.Now().Unix(), 36)
+   value := url.Values{}
+   value.Set("a", now)
+   value.Set("p", "y")
+   value.Set("b", y.tube.VideoId)
+   base, err := get_image(y.tube.VideoId)
+   if err != nil {
+      return nil, err
+   }
+   if base != "" {
+      value.Set("c", base)
+   }
+   play, err := y.tube.Player()
+   if err != nil {
+      return nil, err
+   }
+   var song1 song
+   song1.S = play.VideoDetails.Author + " - " + play.VideoDetails.Title
+   fmt.Println(play.VideoDetails.ShortDescription)
+   value.Set("y", strconv.Itoa(
+      play.Microformat.PlayerMicroformatRenderer.PublishDate[0].Year(),
+   ))
+   song1.Q = value.Encode()
+   return &song1, nil
+}
+
+func new_youtube() *youtube_set {
+   set.f.StringVar(&set.tube.VideoId, "b", "", "video ID")
+   set.tube.Context.Client.ClientName = "WEB"
+}
+
+func (i *InnerTube) Player() (*Player, error) {
+   type InnerTube struct {
+      ContentCheckOk bool `json:"contentCheckOk,omitempty"`
+      Context        struct {
+         Client struct {
+            AndroidSdkVersion int    `json:"androidSdkVersion"`
+            ClientName        string `json:"clientName"`
+            ClientVersion     string `json:"clientVersion"`
+            OsVersion         string `json:"osVersion"`
+         } `json:"client"`
+      } `json:"context"`
+      RacyCheckOk bool   `json:"racyCheckOk,omitempty"`
+      VideoId     string `json:"videoId"`
+   }
+   i.Context.Client.AndroidSdkVersion = 32
+   i.Context.Client.OsVersion = "12"
+   i.Context.Client.ClientVersion = web_version
+   data, err := json.Marshal(i)
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://www.youtube.com/youtubei/v1/player",
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("user-agent", user_agent+i.Context.Client.ClientVersion)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   play := &Player{}
+   err = json.NewDecoder(resp.Body).Decode(play)
+   if err != nil {
+      return nil, err
+   }
+   return play, nil
+}
+
+func get_image(video_id string) (string, error) {
+   var imgs []youtube.YtImg
+   for _, img := range youtube.YtImgs {
+      if img.Height < 720 {
+         imgs = append(imgs, img)
+      }
+   }
+   sort.SliceStable(imgs, func(a, b int) bool {
+      com := imgs[a].Height - imgs[b].Height
+      if com != 0 {
+         return com >= 1
+      }
+      def := func(i int) int {
+         return strings.Index(imgs[i].Name, "default")
+      }
+      com = def(a) - def(b)
+      if com != 0 {
+         return com >= 1
+      }
+      def = func(i int) int {
+         return strings.Index(imgs[i].Name, "webp")
+      }
+      return def(b) < def(a)
+   })
+   for index, img := range imgs {
+      img.VideoId = video_id
+      address := img.String()
+      fmt.Println(address)
+      resp, err := http.Head(address)
+      if err != nil {
+         return "", err
+      }
+      if resp.StatusCode == http.StatusOK {
+         if index == 0 {
+            return "", nil
+         }
+         return path.Base(address), nil
+      }
+   }
+   return "", nil
+}
+
+func (y *YtImg) String() string {
+   var b strings.Builder
+   b.WriteString("http://i.ytimg.com/vi")
+   if strings.HasSuffix(y.Name, ".webp") {
+      b.WriteString("_webp")
+   }
+   b.WriteByte('/')
+   b.WriteString(y.VideoId)
+   b.WriteByte('/')
+   b.WriteString(y.Name)
+   return b.String()
+}
