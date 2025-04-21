9c39693a0756d1c17ec8196d61fcd2e2fa3380fe
367 <367@tuta.io>
2025-04-20 21:37:57 -0500

    new file:   go/nintendo/nintendo.go

---
+++ go/nintendo/nintendo.go
@@ -0,0 +1,491 @@
+package nintendo
+
+var Games = []game{
+   {
+      name: "Donkey Kong Country 2",
+      link: []link{
+         {url: "wikipedia.org/wiki/Donkey_Kong_Country_2"},
+         {
+            url:  "youtube.com/playlist?list=PLKNuUcvZGX23N53oLt93q4CZRj8m-Il_-",
+            text: "Jammin' Sam Miller",
+         },
+      },
+      track: []track{
+         {
+            number: 15,
+            name:   "mining melancholy",
+            link: []link{
+               {
+                  url:  "youtube.com/watch?v=cBK2YIrY62I",
+                  text: "Donkey Kong Country 2 - Mining Melancholy [Restored]",
+               },
+               {
+                  url:  "youtube.com/watch?v=qpJbv1JEA4I",
+                  text: "Donkey Kong Country 2 - Mining Melancholy [Restored] 2023 Mix",
+               },
+            },
+         },
+         {
+            number: 19,
+            name:   "stickerbush symphony",
+            link: []link{
+               {
+                  url:  "youtube.com/watch?v=qIk6YFTzckc",
+                  text: "Donkey Kong Country 2 - Stickerbush Symphony [Restored]",
+               },
+               {
+                  url:  "youtube.com/watch?v=TxP2nqBIC44",
+                  text: "Donkey Kong Country 2 - Stickerbush Symphony [Restored] 2024 Mix",
+               },
+            },
+         },
+         {
+            number: 23,
+            name:   "forest interlude",
+            link: []link{
+               {
+                  url:  "youtube.com/watch?v=wxAZfLenfa0",
+                  text: "Donkey Kong Country 2 - Forest Interlude [Restored]",
+               },
+               {
+                  url:  "youtube.com/watch?v=AZOZXoDwCzI",
+                  text: "Donkey Kong Country 2 - Forest Interlude [Restored] 2025 Mix",
+               },
+            },
+         },
+      },
+   },
+   {
+      name: "Mega Man X (video game)",
+      link: []link{
+         {url: "wikipedia.org/wiki/Mega_Man_X_(video_game)"},
+         {
+            text: "Frygoon",
+            url:  "youtube.com/playlist?list=PLLVedurB7ZPbdTRgvL53QN_0o8bWO9vL3",
+         },
+      },
+      track: []track{
+         {
+            number: 12,
+            name:   "Chill Penguin (Snow Mountain Stage)",
+            link: []link{
+               {
+                  url:  "youtube.com/watch?v=fcOjHuAD6SI",
+                  text: "Mega Man X OST - T12: Chill Penguin (Snow Mountain Stage)",
+               },
+               {
+                  url:  "youtube.com/watch?v=etuuQyKuel4",
+                  text: "Mega Man X1 - Chill Peguin Stage(Jazz Remix)",
+               },
+            },
+         },
+      },
+   },
+   {
+      name: "Pilotwings 64",
+      link: []link{
+         {url: "wikipedia.org/wiki/Pilotwings_64"},
+         {
+            url:  "youtube.com/playlist?list=PLWsnao9n727OfUwLfkm1bJjnkwxLNcSNH",
+            text: "Pilotwings 64 HD",
+         },
+         {
+            url:  "youtube.com/playlist?list=PLWsnao9n727MUC1DSLXttRIASGxLmTRQb",
+            text: "Pilotwings 64 HD Ultra",
+         },
+         {
+            url:  "youtube.com/playlist?list=PLWsnao9n727NrHIiqnZEtD180s9qxL4zl",
+            text: "Pilotwings 64 Retro HD+",
+         },
+      },
+      track: []track{
+         {
+            number: 5,
+            name:   "Hang Glider",
+            link: []link{
+               {
+                  url:  "youtube.com/watch?v=h8-OfKFPnjw",
+                  text: "Pilotwings 64: Hang Glider",
+               },
+               {
+                  url:  "youtube.com/watch?v=nv69iNJbqKo",
+                  text: "Pilotwings 64: Hang Glider HD",
+               },
+               {
+                  url:  "youtube.com/watch?v=aaInC-vCjt0",
+                  text: "Pilotwings 64: Hang Glider Retro HD+",
+               },
+            },
+         },
+         {
+            number: 9,
+            name:   "Rocket Belt",
+            link: []link{
+               {
+                  url:  "youtube.com/watch?v=sAtiUoPB7d4",
+                  text: "Pilotwings 64: Rocket Belt",
+               },
+               {
+                  url:  "youtube.com/watch?v=1QI0zjNZJtU",
+                  text: "Pilotwings 64: Rocket Belt HD",
+               },
+               {
+                  url:  "youtube.com/watch?v=R9u06H6j8a0",
+                  text: "Pilotwings 64: Rocket Belt Retro HD+",
+               },
+            },
+         },
+         {
+            number: 20,
+            name:   "Sky Diving",
+            link: []link{
+               {
+                  url:  "youtube.com/watch?v=NDlyfygPZDs",
+                  text: "Pilotwings 64: Sky Diving",
+               },
+               {
+                  url:  "youtube.com/watch?v=uARj6X14qE8",
+                  text: "Pilotwings 64: Sky Diving HD",
+               },
+               {
+                  url:  "youtube.com/watch?v=cbb0ubUAJqc",
+                  text: "Pilotwings 64: Sky Diving Retro HD+",
+               },
+            },
+         },
+         {
+            number: 26,
+            name:   "Birdman",
+            link: []link{
+               {
+                  url:  "youtube.com/watch?v=DROiSgvFsRY",
+                  text: "Pilotwings 64: Birdman",
+               },
+               {
+                  url:  "youtube.com/watch?v=bsUNUqk-JHg",
+                  text: "Pilotwings 64: Birdman HD",
+               },
+               {
+                  url:  "youtube.com/watch?v=ar8oGTSg5Lk",
+                  text: "Pilotwings 64: Birdman Retro HD+",
+               },
+            },
+         },
+         {
+            number: 31,
+            name:   "Bravissimo!",
+            link: []link{
+               {
+                  url:  "youtube.com/watch?v=mziDsd_SE0M",
+                  text: "Pilotwings 64: Bravissimo!",
+               },
+               {
+                  url:  "youtube.com/watch?v=mdHjSThrRYc",
+                  text: "Pilotwings 64: Bravissimo! HD",
+               },
+               {
+                  url:  "youtube.com/watch?v=qzT0yKHN1f8",
+                  text: "Pilotwings 64: Bravissimo! Retro HD+",
+               },
+            },
+         },
+      },
+   },
+   {
+      name: "Star Fox 64",
+      link: []link{
+         {url: "archive.org/details/starfox-64-soundtrack-cd"},
+         {url: "wikipedia.org/wiki/Star_Fox_64"},
+      },
+      track: []track{
+         {
+            number: 1,
+            name:   "opening theme",
+            link: []link{
+               {url: "archive.org/details/starfox-64-soundtrack-cd/01+-+Opening+Theme.flac"},
+            },
+         },
+         {
+            number: 2,
+            name:   "title theme",
+            link: []link{
+               {url: "archive.org/details/starfox-64-soundtrack-cd/02+-+Title+Theme.flac"},
+            },
+         },
+         {
+            number: 3,
+            name:   "select theme",
+            link: []link{
+               {url: "archive.org/details/starfox-64-soundtrack-cd/03+-+Select+Theme.flac"},
+            },
+         },
+      },
+   },
+   {
+      name: "Super Mario 64",
+      link: []link{
+         {url: "google.com/search?q=Koji+Kondo+Super+Mario+64"},
+         {url: "wikipedia.org/wiki/Super_Mario_64"},
+         {
+            url:  "youtube.com/playlist?list=PLB4DC2508C4D0076D",
+            text: "RadiatorRampardos",
+         },
+         {
+            url:  "youtube.com/playlist?list=PLu0fouVDrRZM1j1U4lTBgNnLbEX4R4Nld",
+            text: "SilvaGunner",
+         },
+      },
+      track: []track{
+         {
+            number: 9,
+            name:   "Dire, Dire Docks",
+            link: []link{
+               {
+                  url:  "youtube.com/watch?v=Zqa2mgjbOIM",
+                  text: "RadiatorRampardos",
+               },
+               {
+                  url:  "youtube.com/watch?v=XRh0Z36RrWY",
+                  text: "Relaxing Piano - Super Mario 64 Underwater Theme",
+               },
+               {
+                  url:  "youtube.com/watch?v=N6G7uFlUyn4",
+                  text: "SilvaGunner",
+               },
+            },
+         },
+         {
+            number: 35,
+            name:   "staff roll",
+            link: []link{
+               {
+                  url:  "youtube.com/watch?v=o-xRxXYpavU",
+                  text: "RadiatorRampardos",
+               },
+               {
+                  url:  "youtube.com/watch?v=_UmksF3MFXg",
+                  text: "SilvaGunner",
+               },
+            },
+         },
+      },
+   },
+   {
+      name: "Super Mario Galaxy 2",
+      link: []link{
+         {url: "wikipedia.org/wiki/Super_Mario_Galaxy_2"},
+         {
+            url:  "youtube.com/playlist?list=PLC583980D7806298E",
+            text: "RadiatorRampardos",
+         },
+      },
+      track: []track{
+         {
+            number: 27,
+            name:   "Cosmic Cove Galaxy",
+            link: []link{
+               {url: "youtube.com/watch?v=zcV7RptHqro"},
+            },
+         },
+      },
+   },
+   {
+      name: "Super Mario World",
+      link: []link{
+         {url: "wikipedia.org/wiki/Super_Mario_World"},
+         {
+            url:  "youtube.com/playlist?list=PLu0fouVDrRZNXc8DkkZR2Sx9N5wuotyrO",
+            text: "SilvaGunner",
+         },
+      },
+      track: []track{
+         {
+            number: 3,
+            name:   "donut plains",
+            link: []link{
+               {url: "youtube.com/watch?v=sMqzn_bqK1Q"},
+            },
+         },
+      },
+   },
+   {
+      name: "The Legend of Zelda: Majora's Mask",
+      link: []link{
+         {url: "wikipedia.org/wiki/The_Legend_of_Zelda:_Majora's_Mask"},
+         {url: "zeldauniverse.net/media/music/majoras-mask-original-soundtrack"},
+      },
+      track: []track{
+         {
+            number: 19,
+            name:   "cremia's carriage",
+            link: []link{
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/majorasmaskost/cd2/2-19+Cremias+Carriage.mp3"},
+            },
+         },
+      },
+   },
+   {
+      name: "The Legend of Zelda: Ocarina of Time",
+      link: []link{
+         {url: "google.com/search?q=Koji+Kondo+The+Legend+of+Zelda:+Ocarina+of+Time"},
+         {url: "wikipedia.org/wiki/The_Legend_of_Zelda:_Ocarina_of_Time"},
+         {url: "zeldauniverse.net/media/music/ocarina-of-time-original-soundtrack"},
+         {
+            text: "Cha Chingo Christiano",
+            url:  "youtube.com/playlist?list=PLESFnlO3kNnpqtdBGrIl58Nn9OVzxhgLl",
+         },
+      },
+      track: []track{
+         {
+            number: 1,
+            name:   "title theme",
+            link: []link{
+               {url: "youtube.com/watch?v=_dPCK3IPa7k"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/ocarinaoftimeost/01+-+Title+Theme.mp3"},
+            },
+         },
+         {
+            number: 6,
+            name:   "kokiri forest",
+            link: []link{
+               {url: "youtube.com/watch?v=rdaJ5pJmNfo"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/ocarinaoftimeost/06+-+Kokiri+Forest.mp3"},
+            },
+         },
+         {
+            number: 12,
+            name:   "inside the deku tree",
+            link: []link{
+               {url: "youtube.com/watch?v=WomOOpj-8o8"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/ocarinaoftimeost/12+-+Inside+The+Deku+Tree.mp3"},
+            },
+         },
+         {
+            number: 19,
+            name:   "hyrule field theme",
+            link: []link{
+               {url: "youtube.com/watch?v=4h6kO9HVMaw"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/ocarinaoftimeost/19+-+Hyrule+Field+Theme.mp3"},
+            },
+         },
+         {
+            number: 28,
+            name:   "lon lon ranch",
+            link: []link{
+               {url: "youtube.com/watch?v=q5Wq8DoYEmU"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/ocarinaoftimeost/28+-+Lon+Lon+Ranch.mp3"},
+            },
+         },
+         {
+            number: 30,
+            name:   "kakariko village",
+            link: []link{
+               {url: "youtube.com/watch?v=rS-Si8vsfbk"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/ocarinaoftimeost/30+-+Kakariko+Village.mp3"},
+            },
+         },
+         {
+            number: 39,
+            name:   "zora's domain",
+            link: []link{
+               {url: "youtube.com/watch?v=JeJC2fxob0g"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/ocarinaoftimeost/39+-+Zoras+Domp3"},
+            },
+         },
+         {
+            number: 40,
+            name:   "great fairy fountain",
+            link: []link{
+               {url: "youtube.com/watch?v=C3xHPcWxzXM"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/ocarinaoftimeost/40+-+Great+Fairy+Fountain.mp3"},
+            },
+         },
+         {
+            number: 41,
+            name:   "potion shop",
+            link: []link{
+               {url: "youtube.com/watch?v=_UN8OcTfOVA"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/ocarinaoftimeost/41+-+Potion+Shop.mp3"},
+            },
+         },
+         {
+            number: 44,
+            name:   "temple of time",
+            link: []link{
+               {url: "youtube.com/watch?v=Sv66WYSQPAc"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/ocarinaoftimeost/44+-+Temple+Of+Time.mp3"},
+            },
+         },
+         {
+            number: 50,
+            name:   "sheik's theme",
+            link: []link{
+               {url: "youtube.com/watch?v=3iV8MkbGm2o"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/ocarinaoftimeost/50+-+Sheiks+Theme.mp3"},
+            },
+         },
+         {
+            number: 57,
+            name:   "windmill hut",
+            link: []link{
+               {url: "youtube.com/watch?v=ms9-vyV0HNo"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/ocarinaoftimeost/57+-+Windmill+Hut.mp3"},
+            },
+         },
+         {
+            number: 68,
+            name:   "gerudo valley",
+            link: []link{
+               {url: "youtube.com/watch?v=gmwdOH6fJ1s"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/ocarinaoftimeost/68+-+Gerudo+Valley.mp3"},
+            },
+         },
+      },
+   },
+   {
+      name: "The Legend of Zelda: The Wind Waker",
+      link: []link{
+         {url: "wikipedia.org/wiki/The_Legend_of_Zelda:_The_Wind_Waker"},
+         {url: "zeldauniverse.net/media/music/the-wind-waker-original-soundtrack"},
+         {
+            text: "Rabbit OST",
+            url:  "youtube.com/playlist?list=PLH834K1pkcjJRwrw6j-Ah3FL48SDAsK_z",
+         },
+      },
+      track: []track{
+         {
+            number: 3,
+            name:   "the legendary hero",
+            link: []link{
+               {url: "youtube.com/watch?v=jNuLYOMwdS0"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/thewindwakerost/cd1/The_Legend_of_Zelda_-_The_Wind_Waker_-_03_-_Legendary_Hero.mp3"},
+            },
+         },
+         {
+            number: 38,
+            name:   "dragon roost island",
+            link: []link{
+               {url: "youtube.com/watch?v=02pp7Pb7Bls"},
+               {url: "zeldauniverse.s3.amazonaws.com/soundtracks/thewindwakerost/cd1/The_Legend_of_Zelda_-_The_Wind_Waker_-_38_-_Dragon_Island.mp3"},
+            },
+         },
+      },
+   },
+}
+
+type game struct {
+   name  string
+   link  []link
+   track []track
+}
+
+type track struct {
+   number int
+   name   string
+   link   []link
+}
+
+type link struct {
+   text string
+   url  string
+}
