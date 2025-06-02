11a8b40ab40218a72db3b3016159413c1a0cbed0
367 <367@tuta.io>
2025-06-02 06:08:25 -0500

    +var KellyMoran = []album{

--- go/music/music.go
+++ go/music/music.go
@@ -1,28 +1,32 @@
 package music
 
-var Albums = []album{
+var KellyMoran = []album{
    {
-      name: "donkey kong country",
+      name: "helix (edit)",
       link: []link{
-         {url: "wikipedia.org/wiki/Donkey_Kong_Country"},
-         {
-            text: "Jammin' Sam Miller",
-            url:  "youtube.com/playlist?list=PLKNuUcvZGX21obTJzXuVHUWRcS2cCBBsP",
-         },
+         {url: "youtube.com/playlist?list=OLAK5uy_kB2WFbIR3N8VhQWq0G94nUUoa1275EcMU"},
       },
       track: []track{
          {
-            number: 7,
-            name:   "aquatic ambience",
+            number: 1,
+            name:   "helix (edit)",
             link: []link{
-               {
-                  text: "Aquatic Ambience [Restored]",
-                  url:  "youtube.com/watch?v=-5rAjOjTGtc",
-               },
-               {
-                  text: "Aquatic Ambience [Restored] [2023 Mix]",
-                  url:  "youtube.com/watch?v=39hGqV42CkM",
-               },
+               {url: "youtube.com/watch?v=tdeZ4ecFaxE"},
+            },
+         },
+      },
+   },
+   {
+      name: "ultraviolet",
+      link: []link{
+         {url: "youtube.com/playlist?list=OLAK5uy_mQz8ftbRzC-chKW0YSJ6yyPmgdrHv12CI"},
+      },
+      track: []track{
+         {
+            number: 1,
+            name:   "autowave",
+            link: []link{
+               {url: "youtube.com/watch?v=vD1m4flw_Ko"},
             },
          },
       },
