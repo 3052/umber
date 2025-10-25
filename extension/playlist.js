992a3885c65d8ede96797433788235117a85eed8
27 <27@riseup.net>
2025-10-25 00:11:59 -0500

    +  "S": "ARIatHOME - old school freestyler makes me turn off my drill beat 🔥😂 @gospelgabe1978"

--- extension/playlist.js
+++ extension/playlist.js
@@ -1,14 +1,37 @@
 'use strict';
 
-function append(msg) {
+function seekToSeconds() {
+   const seconds = parseInt(timeInputElement.value, 10);
+   console.log(seconds);
+   document.querySelector('video').currentTime = seconds;
+}
+
+const timeInputElement = document.getElementById('timeInput');
+
+timeInputElement.addEventListener('keydown', function(event) {
+   if (event.key === 'Enter') {
+      seekToSeconds();
+   }
+});
+
+const main = document.querySelector('main');
+
+browser.runtime.onMessage.addListener(function(event) {
+   const temp = document.querySelector('template');
    const fig = temp.content.firstElementChild.cloneNode(true);
    main.append(fig);
    const vid = fig.querySelector('video');
    // src
-   vid.src = msg.src;
-   vid.onended = next;
+   vid.src = event.src;
+   vid.onended = function() {
+      main.querySelector('figure').remove();
+      const vid = main.querySelector('video');
+      if (vid !== null) {
+         vid.play();
+      }
+   };
    // poster
-   vid.poster = msg.poster;
+   vid.poster = event.poster;
    // If you dont start playback within 30 seconds, then YouTube blocks playback.
    // `preload` doesnt help anything. `load()` doesnt help anything.
    vid.play();
@@ -16,17 +39,5 @@ function append(msg) {
       vid.pause();
    }, 99);
    // title
-   fig.querySelector('figcaption').textContent = msg.title;
-}
-
-function next() {
-   main.querySelector('figure').remove();
-   const vid = main.querySelector('video');
-   if (vid !== null) {
-      vid.play();
-   }
-}
-
-const main = document.querySelector('main');
-const temp = document.querySelector('template');
-browser.runtime.onMessage.addListener(append);
+   fig.querySelector('figcaption').textContent = event.title;
+});
