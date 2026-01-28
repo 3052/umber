1bb12b34f6504f018a7d4d3bb6bd7b49ff769e04
27 <27@riseup.net>
2026-01-28 17:01:57 -0600

    deleted:    extension/assets/umber.png

--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,45 @@
 'use strict';
 
+/* --- YOUTUBE --- */
+async function youTube() {
+   const ref = new URL(this.href);
+   const req = {};
+   const body = {};
+   body.videoId = ref.searchParams.get('v');
+   body.context = {};
+   body.context.client = {};
+   req.headers = {};
+   //////////////////////////////////////////////////////////////////////////////
+   req.headers['X-Goog-Visitor-Id'] = 'CgtNbzlJR19GY24tNCjl_pDABjIKCgJVUxIEGgAgDA==';
+   body.context.client.clientName = 'ANDROID';
+   body.context.client.clientVersion = '21.02.35';
+   //////////////////////////////////////////////////////////////////////////////
+   req.body = JSON.stringify(body);
+   req.method = 'POST';
+   const resp = await fetch('https://www.youtube.com/youtubei/v1/player', req);
+   const play = await resp.json();
+   const msg = {
+      poster: this.querySelector('img').src,
+      title: play.videoDetails.author + ' - ' + play.videoDetails.title
+   };
+   if (play.playabilityStatus.status == 'OK') {
+      play.streamingData.adaptiveFormats.sort(
+         (a, b) => b.bitrate - a.bitrate
+      );
+      for (const form of play.streamingData.adaptiveFormats) {
+         // some videos do not offer WebM: 6_lMeEMMbyY
+         if (form.audioQuality == 'AUDIO_QUALITY_MEDIUM') {
+            msg.src = form.url;
+            break;
+         }
+      }
+   } else {
+      msg.src = '';
+      msg.title += ' - ' + play.playabilityStatus.reason;
+   }
+   browser.runtime.sendMessage(msg);
+}
+
 /* --- BANDCAMP --- */
 async function bandcamp() {
    let ref = new URL('https://bandcamp.com/api/mobile/24/tralbum_details');
@@ -54,46 +94,6 @@ async function soundCloud() {
       poster: this.querySelector('img').src,
       title: this.parentNode.querySelector('td').textContent
    });
-}
-
-/* --- YOUTUBE --- */
-async function youTube() {
-   const ref = new URL(this.href);
-   const req = {};
-   const body = {};
-   body.videoId = ref.searchParams.get('v');
-   body.context = {};
-   body.context.client = {};
-   req.headers = {};
-   //////////////////////////////////////////////////////////////////////////////
-   req.headers['X-Goog-Visitor-Id'] = 'CgtNbzlJR19GY24tNCjl_pDABjIKCgJVUxIEGgAgDA==';
-   body.context.client.clientName = 'ANDROID';
-   body.context.client.clientVersion = '20.34.37';
-   //////////////////////////////////////////////////////////////////////////////
-   req.body = JSON.stringify(body);
-   req.method = 'POST';
-   const resp = await fetch('https://www.youtube.com/youtubei/v1/player', req);
-   const play = await resp.json();
-   const msg = {
-      poster: this.querySelector('img').src,
-      title: play.videoDetails.author + ' - ' + play.videoDetails.title
-   };
-   if (play.playabilityStatus.status == 'OK') {
-      play.streamingData.adaptiveFormats.sort(
-         (a, b) => b.bitrate - a.bitrate
-      );
-      for (const form of play.streamingData.adaptiveFormats) {
-         // some videos do not offer WebM: 6_lMeEMMbyY
-         if (form.audioQuality == 'AUDIO_QUALITY_MEDIUM') {
-            msg.src = form.url;
-            break;
-         }
-      }
-   } else {
-      msg.src = '';
-      msg.title += ' - ' + play.playabilityStatus.reason;
-   }
-   browser.runtime.sendMessage(msg);
 }
 
 /* --- DELAY / MAIN --- */
